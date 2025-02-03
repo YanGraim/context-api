40342855db38c73f008ca25d9c35afd59b4fc998
YanGraim <yangraim@gmail.com>
2025-02-03 12:16:19 -0300

    cria component footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+export function Footer() {
+    return (
+        <footer>
+            <hr />
+            <h4>Alunos online na plataforma: 190</h4>
+            <button onClick={() => {}}>
+                Novo aluno
+            </button>
+        </footer>
+    )
+}
